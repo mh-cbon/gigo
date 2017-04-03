de9acd7898739d816189ddadc55180bee37b654d
mh-cbon <mh-cbon@users.noreply.github.com>
2017-04-03 14:55:46 +0200

    dump: improve output

--- interpreter/generic/dump.go
+++ interpreter/generic/dump.go
@@ -13,19 +13,44 @@ func Dump(src Expressioner) {
 }
 func dump(src Expressioner, lvl int) {
 	x := strings.Repeat(" ", lvl)
-	fmt.Printf("%v%-6v %-20T Tokens(%v)\n", x, "begin", src, len(src.GetExprs()))
+	T := fmt.Sprintf("%T", src)
+	T = strings.Replace(T, "*glang.", "", -1)
 	exprs := src.GetExprs()
-	for _, e := range exprs {
+	for i, e := range exprs {
 		if len(e.GetExprs()) == 0 {
 			tok := e.GetTokens()[0]
-			fmt.Printf("%40v %v %20v %q\n", x, tok.GetPos().String(),
+			yy := ""
+			if len(src.GetExprs()) == 1 {
+				yy = fmt.Sprintf("=> %v %v token", T, len(src.GetExprs()))
+			} else if i == 0 {
+				yy = fmt.Sprintf("-> %v %v tokens", T, len(src.GetExprs()))
+			} else if i == len(src.GetExprs())-1 {
+				yy = fmt.Sprintf("<- %v", T)
+			}
+			fmt.Printf("%-40v", x+yy)
+
+			fmt.Printf("%-8v%-20v %q\n",
+				tok.GetPos().String(),
 				glanglexer.TokenType(tok.GetType()),
 				tok.GetValue())
 		} else {
+			if i == 0 {
+				yy := fmt.Sprintf("-> %v %v tokens", T, len(src.GetExprs()))
+				fmt.Printf("%-40v\n", x+yy)
+			}
 			dump(e, lvl+1)
+			if i == len(src.GetExprs())-1 {
+				yy := fmt.Sprintf("<- %v %v tokens", T, len(src.GetExprs()))
+				fmt.Printf("%-40v\n", x+yy)
+			}
 		}
 	}
-	fmt.Printf("%v%-6v %-20T tokens(%v)\n", x, "end", src, len(src.GetExprs()))
+
+	if len(src.GetExprs()) < 1 || len(src.GetExprs()) > 2000 {
+		yy := fmt.Sprintf("<- %v %v tokens", T, len(src.GetExprs()))
+		fmt.Printf("%-40v\n", x+yy)
+	}
+	// fmt.Printf("%v%-6v %-20T tokens(%v)\n", x, "<-", src, len(src.GetExprs()))
 }
 
 // DumpTokens prints a list of tokens
