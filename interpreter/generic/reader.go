89c8986a882783deb6b6c43121ba9a17290e5a7d
mh-cbon <mh-cbon@users.noreply.github.com>
2017-04-10 19:26:16 +0200

    buffered prettyprint reader, comments, rename interface name

--- interpreter/generic/reader.go
+++ interpreter/generic/reader.go
@@ -9,20 +9,25 @@ import (
 	lexer "github.com/mh-cbon/state-lexer"
 )
 
-type TokenReaderOK interface {
+// TokenReader reads *lexer.Token
+type TokenReader interface {
 	NextToken() *lexer.Token
 }
 
-type TokenerReaderOK interface {
+// TokenerReader reads Tokener
+type TokenerReader interface {
 	NextToken() Tokener
 }
 
+// ReadTokenWithPos reads lexer.Token, outputs Tokener
 type ReadTokenWithPos struct {
-	Reader TokenReaderOK
+	Reader TokenReader
 	line   int
 	pos    int
 }
 
+// NextToken advance to the next token.
+// returns nil on eof.
 func (r *ReadTokenWithPos) NextToken() Tokener {
 	if next := r.Reader.NextToken(); next != nil {
 		tok := NewTokenWithPos(*next, r.line, r.pos)
@@ -36,7 +41,8 @@ func (r *ReadTokenWithPos) NextToken() Tokener {
 	return nil
 }
 
-func NewReadTokenWithPos(r TokenReaderOK) *ReadTokenWithPos {
+// NewReadTokenWithPos initializes a TokenerReader from a TokenerReader
+func NewReadTokenWithPos(r TokenReader) *ReadTokenWithPos {
 	return &ReadTokenWithPos{
 		Reader: r,
 		line:   1,
@@ -44,27 +50,29 @@ func NewReadTokenWithPos(r TokenReaderOK) *ReadTokenWithPos {
 	}
 }
 
+// ReadNPrettyPrint is a TokenerReader that pretty prints what it reads.
 type ReadNPrettyPrint struct {
-	Reader TokenerReaderOK
+	Reader TokenerReader
 	Namer  TokenerNamer
 	w      *tabwriter.Writer
 	d      bool
 }
 
+// Flush prints on the underlying write.
 func (r *ReadNPrettyPrint) Flush() {
 	r.w.Flush()
 }
+
+// NextToken returns next token and prints it.
 func (r *ReadNPrettyPrint) NextToken() Tokener {
 	if !r.d {
 		r.d = true
 		fmt.Fprintf(r.w, "%v\t %v\t %q\n", "Line:Pos", "TokenName:tok.Type", "tok.Value")
-		r.Flush()
 	}
 	if next := r.Reader.NextToken(); next != nil {
 		fmt.Fprintf(r.w, "%0000d:%000d\t %v:%000d\t %q\n",
 			next.GetPos().Line,
 			next.GetPos().Pos, r.Namer(next), next.GetType(), next.GetValue())
-		r.Flush()
 		return next
 	} else if next == nil {
 		fmt.Fprintf(r.w, "%v\t %v\t %q\n", "Line:Pos", "TokenName:tok.Type", "tok.Value")
@@ -73,7 +81,8 @@ func (r *ReadNPrettyPrint) NextToken() Tokener {
 	return nil
 }
 
-func NewReadNPrettyPrint(r TokenerReaderOK, Namer TokenerNamer, out io.Writer) *ReadNPrettyPrint {
+// NewReadNPrettyPrint initializes a prety printer reader to out, Namer resolves token types.
+func NewReadNPrettyPrint(r TokenerReader, Namer TokenerNamer, out io.Writer) *ReadNPrettyPrint {
 	return &ReadNPrettyPrint{
 		Reader: r,
 		Namer:  Namer,
@@ -96,48 +105,3 @@ func TokenerName(h TokenNamer) TokenerNamer {
 
 // TokenTyper gives the name of a Token type.
 type TokenTyper func(lexer.TokenType) string
-
-// ------------------
-// old stuff
-
-// // TokenReader return Token or nil.
-// type TokenReader func() *lexer.Token
-//
-// // TokenerReader return Tokener or nil.
-// type TokenerReader func() Tokener
-//
-// // PositionnedTokenReader turns a TokenReader into a feed of Tokener.
-// func PositionnedTokenReader(reader TokenReader) func() Tokener {
-// 	var line = 1
-// 	var pos = 0
-// 	return func() Tokener {
-// 		if next := reader(); next != nil {
-// 			tok := NewTokenWithPos(*next, line, pos)
-// 			if k := strings.Count(tok.Value, "\n"); k > 0 {
-// 				line += k
-// 				pos = -1
-// 			}
-// 			pos += len(tok.Value)
-// 			return tok
-// 		}
-// 		return nil
-// 	}
-// }
-//
-// // PrettyPrint a feed of token being read.
-// func PrettyPrint(reader TokenerReader, namer TokenerNamer) func() Tokener {
-// 	w := tabwriter.NewWriter(os.Stdout, 0, 0, 1, ' ', tabwriter.Debug)
-// 	fmt.Fprintf(w, "%v\t %v\t %q\n", "Line:Pos", "TokenName:tok.Type", "tok.Value")
-// 	return func() Tokener {
-// 		if next := reader(); next != nil {
-// 			fmt.Fprintf(w, "%0000d:%000d\t %v:%000d\t %q\n",
-// 				next.GetPos().Line,
-// 				next.GetPos().Pos, namer(next), next.GetType(), next.GetValue())
-// 			return next
-// 		} else if next == nil {
-// 			fmt.Fprintf(w, "%v\t %v\t %q\n", "Line:Pos", "TokenName:tok.Type", "tok.Value")
-// 			w.Flush()
-// 		}
-// 		return nil
-// 	}
-// }
